84fe079a56549e6aad1692e255f4f9385a57ac26
Georgy <goshan31458@gmail.com>
2020-05-21 19:12:35 +0900

    Добавление store = {} в state.js. ООП. Проблема с контекстом через bind()

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import * as serviceWorker from './serviceWorker';
-import state, { subs } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostText } from './redux/state';
+
+let state = store.getState();
+
 
 let rerenderTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App updateNewPostText={updateNewPostText} addPost={addPost} appState={state} />
+            <App updateNewPostText={store.updateNewPostText.bind(store)} addPost={store.addPost.bind(store)} appState={state} />
         </React.StrictMode>,
         document.getElementById('root')
     );
@@ -17,7 +19,7 @@ let rerenderTree = (state) => {
 
 rerenderTree(state);
 
-subs(rerenderTree);
+store.subs(rerenderTree);
 
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,61 +1,65 @@
-let rerenderTree = () => {
-    console.log('State was change')
-}
 
-let state = {
-    profilePage:{
-        postsData: [
-            {id: 1, message: "I think it's great!", likesCount: 35},
-            {id: 2, message: "Hi! how are you?", likesCount: 27},
-            {id: 3, message: "I'm gonna sleep, who with me?", likesCount: 27},
-            {id: 4, message: "Today is sunny", likesCount: 27},
-            {id: 5, message: "I think, I have wil great anything.", likesCount: 27},
-            {id: 6, message: "Hi! how are you?", likesCount: 27},
-            {id: 7, message: "It's my first postaret", likesCount: 58}
-        ],
-        newPostText: 'it-samurai'
+let store = {
+    _state: {
+        profilePage:{
+            postsData: [
+                {id: 1, message: "I think it's great!", likesCount: 35},
+                {id: 2, message: "Hi! how are you?", likesCount: 27},
+                {id: 3, message: "I'm gonna sleep, who with me?", likesCount: 27},
+                {id: 4, message: "Today is sunny", likesCount: 27},
+                {id: 5, message: "I think, I have wil great anything.", likesCount: 27},
+                {id: 6, message: "Hi! how are you?", likesCount: 27},
+                {id: 7, message: "It's my first postaret", likesCount: 58}
+            ],
+            newPostText: 'it-samurai'
+        },
+        dialogsPage: {
+            messagesData:[
+                {id: 1, message: "Hi!"},
+                {id: 2, message: "How is your ReacT!"},
+                {id: 3, message: "You!"}
+            ],
+            dialogsData: [
+                {id: 1, name: "John"},
+                {id: 2, name: "Kate"},
+                {id: 3, name: "Margaret"},
+                {id: 4, name: "Ken"},
+                {id: 5, name: "Buch"},
+                {id: 6, name: "Jack"},
+                {id: 7, name: "Rosa"}
+            ]
+        },
+        sidebar:{
+            bestfriends:[
+                {id:1, name: "John", img: 'https://pbs.twimg.com/profile_images/438132278318071808/qIJjSA3h.jpeg'},
+                {id:2, name: "Margaret", img: 'https://yt3.ggpht.com/a/AGF-l78BBd8rWUJ6QL5hINMuCK-A_lbuuC4pYCjBzQ=s900-c-k-c0xffffffff-no-rj-mo'},
+                {id:3, name: "Shon", img: 'https://irs3.4sqi.net/img/general/original/40701818_TB6Ybm5jjWuRNjoyakIqTZPWQ6wf7WfngTUwyn2Vn3U.jpg'}
+            ]
+        }
     },
-    dialogsPage: {
-        messagesData:[
-            {id: 1, message: "Hi!"},
-            {id: 2, message: "How is your ReacT!"},
-            {id: 3, message: "You!"}
-        ],
-        dialogsData: [
-            {id: 1, name: "John"},
-            {id: 2, name: "Kate"},
-            {id: 3, name: "Margaret"},
-            {id: 4, name: "Ken"},
-            {id: 5, name: "Buch"},
-            {id: 6, name: "Jack"},
-            {id: 7, name: "Rosa"}
-        ]
+    _rerenderTree() {
+        console.log('Observer undefined');
     },
-    sidebar:{
-        bestfriends:[
-            {id:1, name: "John", img: 'https://pbs.twimg.com/profile_images/438132278318071808/qIJjSA3h.jpeg'},
-            {id:2, name: "Margaret", img: 'https://yt3.ggpht.com/a/AGF-l78BBd8rWUJ6QL5hINMuCK-A_lbuuC4pYCjBzQ=s900-c-k-c0xffffffff-no-rj-mo'},
-            {id:3, name: "Shon", img: 'https://irs3.4sqi.net/img/general/original/40701818_TB6Ybm5jjWuRNjoyakIqTZPWQ6wf7WfngTUwyn2Vn3U.jpg'}
-        ]
-    }
-};
-
-window.state=state;
-
-export let addPost = () =>{
-    let newPost = {id:8, message: state.profilePage.newPostText, likesCount: 0};
-    state.profilePage.postsData.push(newPost);
-    state.profilePage.newPostText='';
-    rerenderTree(state);
+    getState() {
+        return this._state;
+    },
+    addPost() {
+        let newPost = {id:8, message: this._state.profilePage.newPostText, likesCount: 0};
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText='';
+        this._rerenderTree(this._state);
+    },
+    updateNewPostText(newText){
+        this._state.profilePage.newPostText = newText;
+        this._rerenderTree(this._state);
+    },
+    subs(observer) {
+        this._rerenderTree = observer;;  // observer - наблюдатель, паттерн
+    },
+    
 }
 
-export let updateNewPostText = (newText) =>{
-    state.profilePage.newPostText = newText;
-    rerenderTree(state);
-}
+window.store = store;
 
-export const subs = (observer) =>{
-    rerenderTree = observer;  // observer - наблюдатель, паттерн
-}
 
-export default state;
+export default store;
