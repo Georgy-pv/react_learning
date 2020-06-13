04c32ac6d8ae6dc0e759e90f70a0558edf627dc0
Georgy <goshan31458@gmail.com>
2020-06-13 19:18:41 +0900

    Updates

---
+++ precache-manifest.c9769a2e0620c82d3b113a4e03322c26.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7af1732026debeab824cc0258167a5b",
+    "url": "/react_learning/index.html"
+  },
+  {
+    "revision": "5675f3dd6fb3dc8c1123",
+    "url": "/react_learning/static/css/4.e0d41a7b.chunk.css"
+  },
+  {
+    "revision": "284d4e200d303a05a1ed",
+    "url": "/react_learning/static/css/5.c3d678ce.chunk.css"
+  },
+  {
+    "revision": "a236cbdb315fac3d2a58",
+    "url": "/react_learning/static/css/6.9dc83258.chunk.css"
+  },
+  {
+    "revision": "37e4c39dabf81385ae2f",
+    "url": "/react_learning/static/css/7.5de73870.chunk.css"
+  },
+  {
+    "revision": "e18dd22dcf055afa7506",
+    "url": "/react_learning/static/css/main.8c70a09f.chunk.css"
+  },
+  {
+    "revision": "5cc15b8b0ad2490256c3",
+    "url": "/react_learning/static/js/0.3d0a3a6a.chunk.js"
+  },
+  {
+    "revision": "d2b6d2d271b870f0c8c2",
+    "url": "/react_learning/static/js/3.b2e0a3b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_learning/static/js/3.b2e0a3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5675f3dd6fb3dc8c1123",
+    "url": "/react_learning/static/js/4.ef231229.chunk.js"
+  },
+  {
+    "revision": "284d4e200d303a05a1ed",
+    "url": "/react_learning/static/js/5.c025a4ca.chunk.js"
+  },
+  {
+    "revision": "a236cbdb315fac3d2a58",
+    "url": "/react_learning/static/js/6.03d094ff.chunk.js"
+  },
+  {
+    "revision": "37e4c39dabf81385ae2f",
+    "url": "/react_learning/static/js/7.87945e87.chunk.js"
+  },
+  {
+    "revision": "e18dd22dcf055afa7506",
+    "url": "/react_learning/static/js/main.f99868f4.chunk.js"
+  },
+  {
+    "revision": "5b44eebd6251933b194b",
+    "url": "/react_learning/static/js/runtime-main.a6d98063.js"
+  },
+  {
+    "revision": "1ae12dd4d483ecd20184dc3978c64c76",
+    "url": "/react_learning/static/media/loader.1ae12dd4.svg"
+  }
+]);
